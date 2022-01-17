1d61b1280bf5e966a8981f3118a353a3f8def9a1
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2022-01-17 10:53:54 +0200

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+from module1 import *
+a=0
+eng=[]
+rus=[]
+eng=
+print("Welcome to Dictionary!")
+while True:
+    print("=~     Main Menu    ~=")
+    print("=~     !Actions!    ~=")
+    print("=~    Dictionary    ~=")
+    print("=~      Editing     ~=")
+    print("=~Check your English~=")
+    print("=~Check your Russian~=")
+    Answer=Ask_Input(a)
+    if Answer.lower()=="Dictionary":
---
+++ module1.py
@@ -0,0 +1,6 @@
+def Ask_Input(a):
+    """
+    """
+    a=str(input("=>=>=>"))
+    return a
+def Reader(f:str,l:list):
