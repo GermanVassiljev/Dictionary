9d73cd8156d273f593b556daf07336cebf3dbe25
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2022-01-19 14:14:51 +0200

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,15 +1,38 @@
 from module1 import *
-a=0
+a=" "
 eng=[]
 rus=[]
-eng=
+eng = Reader("eng.txt",eng)
+rus = Reader("rus.txt",rus)
 print("Welcome to Dictionary!")
 while True:
     print("=~     Main Menu    ~=")
     print("=~     !Actions!    ~=")
-    print("=~    Dictionary    ~=")
-    print("=~      Editing     ~=")
+    print("=~   Instructions   ~=")
+    print("=~     Hear Word    ~=")
+    print("=~    Translation   ~=")
+    print("=~     Add Words    ~=")
     print("=~Check your English~=")
     print("=~Check your Russian~=")
     Answer=Ask_Input(a)
-    if Answer.lower()=="Dictionary":
+    if Answer.lower()=="instructions":
+        True
+    elif Answer.lower()=="translation":
+        Translation(eng,rus)
+    elif Answer.lower()=="hear word":
+        print("In what language do you want to hear the word?")
+        lang=Ask_Input(a)
+        print("Word")
+        word=Ask_Input(a)
+        heli(lang,word)
+    elif Answer.lower()=="add":
+        print("Write english word")
+        Word1=Ask_Input(a)
+        New_Word("eng.txt",Word1,eng)
+        print("Write russian translation")
+        Word2=Ask_Input(a)
+        New_Word("rus.txt",Word2,rus)
+    elif Answer.lower()=="check english":
+        True
+    elif Answer.lower()=="check russian":
+        True
